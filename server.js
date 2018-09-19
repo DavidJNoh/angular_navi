fe5df6960d73524bce202c4b2270a4e55408435b
David Noh <davidnoh@mikes-MacBook-Pro-3.local>
2018-09-18 18:51:10 -0700

    w

--- server.js
+++ server.js
@@ -11,13 +11,82 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/basic_mongoose'), {useNewUrlParser: true}
 
 var CakeSchema = new mongoose.Schema({
-    name: {type: String, required:true},
-    url: {type: String, required:true},
-    // rates: [RateSchema]
+    name: {
+        type: String, 
+        required: [true, "Name Can Not Be Empty"],
+        minlength: [2, "Name Too Short"], 
+        maxlength: [4, "Name Too Long"]
+    },
     },{timestamps: true})
 
 mongoose.model('Nav', CakeSchema);
 var Nav = mongoose.model('Nav')
+
+app.get('/all', function(req, res){
+    Nav.find({}, function(err, result){
+        if(err){
+            console.log("Error finding in server", {data: err})
+        }
+        else{
+            res.json({status: true, data: result})
+        }
+    })
+})
+
+app.get('/getOne/:id', function(req, res){
+    Nav.find({_id:req.params.id}, function(err, result){
+        if(err){
+            console.log("Error finding in server")
+            res.json({status:false, data: err})
+        }
+        else{
+            res.json({status: true, data: result})
+        }
+    })
+})
+
+app.post('/add', function(req, res){
+    console.log("Displaying add req.body",req.body)
+    Nav.create(req.body, function(err, result){
+        if(err){
+            console.log("Adding returned error", err["errors"]["name"]["message"]);
+            res.json({status: false, data: err["errors"]["name"]["message"]})
+        }
+        else {
+            res.json({status: true, data: result})
+        }
+    })
+
+})
+
+app.put('/edit/:id', function(req,res){
+    console.log("Displaying edit req.body",req.body)
+    console.log("Displaying edit req.param.id",req.params.id)
+    Nav.updateOne({_id: req.params.id}, {$set: {name:req.body.name}}, function(err){
+        console.log ("??")
+        if(err){
+            console.log("Update Error", err["errors"]["name"]["message"]);
+            res.json({status:false, data: err["errors"]["name"]["message"]})
+        }
+        else{
+            console.log("Updated Successfully")
+            res.json({status:true, data: "Update Success"})
+        }
+    })
+})
+
+app.delete('/delete/:id', function(req, res){
+    console.log(req.params.id)
+    Nav.deleteOne({_id:req.params.id}, function(err, result){
+        if(err){
+            console.log("Returned error", err);
+            res.json({status: false, data: err})
+        }
+        else {
+            res.json({status: true, data: result})
+        }
+    })
+})
 
 app.post('/post', function(req, res){
     console.log(req.body)
